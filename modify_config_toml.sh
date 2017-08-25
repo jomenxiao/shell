a81f43addecd256bba8fb54838abe94b8665daf6
jomenxiao <jomenxiao@gmail.com>
2017-08-25 15:59:53 +0800

    modify config.toml

--- modify_config_toml.sh
+++ modify_config_toml.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 STABILITY_TESTER=$1
-CONFIG_TOML_FILE=$2
+DB_HOST_IP="$(echo $2 |awk -F ":" '{print $2}')"
+DB_HOST_PORT="$(echo $3 |awk -F ":" '{print $2}')"
+DB_USER="$(echo $4 |awk -F ":" '{print $2}')"
+DB_PASSWORD="$(echo $5 |awk -F ":" '{print $2}')"
+
+CONFIG_TOML_FILE=$6
+
 
 
 if [[ ${STABILITY_TESTER} =~ "sysbench|sqllogic_test" ]];then
@@ -13,3 +19,10 @@ else
         -e "/\[serial_suite\]/{n;s/names.*/names = \[ \]/}" \
         ${CONFIG_TOML_FILE}
 fi
+
+
+sed -i  -e "s/host.*/host = \"${DB_HOST_IP}\"/g" \
+        -e "s/port.*/port = \"${DB_HOST_PORT}\"/g" \
+        -e "s/user.*/user = \"${DB_USER}\"/g" \
+        -e "s/password.*/password = \"${DB_PASSWORD}\"/g" \
+            ${CONFIG_TOML_FILE}
